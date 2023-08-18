d2ff177b5d05011a1a1062e643ebaa0ffa8f777c
diegosuna24 <osunaacevedo24@gmail.com>
2023-08-17 20:10:17 -0700

    CODIGO HOLA MUNDO

--- Practica_1Ejercicio1_Osuna_Acevedo.cpp
+++ Practica_1Ejercicio1_Osuna_Acevedo.cpp
@@ -1,1 +1,9 @@
-//isjhdfsdfhsdjf
+//Diego )suna 372273
+#include <stdio.h>
+
+int main() {
+    printf("Hola, mundo!\n");
+    printf("Hola, mundo!\n");
+    printf("Hola, mundo!\n");
+    return 0;
+}
